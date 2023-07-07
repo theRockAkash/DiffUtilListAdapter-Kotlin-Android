dc8b6e5d4c7212bf24a47cb917364db427101068
Akash Verma <121936065+theRockAkash@users.noreply.github.com>
2023-07-07 15:57:01 +0530

    initial

---
+++ NoDataAdapter.kt
@@ -0,0 +1,52 @@
+package com.zapp.app.utils
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.zapp.app.databinding.NoDataCardBinding
+
+class NoDataAdapter : ListAdapter<String, NoDataAdapter.ViewHolder>(DiffCallback()) {
+    class DiffCallback : DiffUtil.ItemCallback<String>() {
+        override fun areItemsTheSame(
+            oldItem: String,
+            newItem: String
+        ): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(
+            oldItem: String,
+            newItem: String
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+    inner class ViewHolder(private val binding: NoDataCardBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: String) {
+            if (item.equals("error"))
+            {
+                binding.ivThumb.hide()
+                binding.ivThumbError.show()
+            }
+            else {
+                binding.ivThumb.show()
+                binding.ivThumbError.hide()
+            }
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val view = NoDataCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+           val item= getItem(position)
+        holder.bind(item)
+    }
+
+
+}
